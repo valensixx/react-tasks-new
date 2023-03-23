dc7f3266250360e565ce840a2a9211999b77cb04
valensixx <valensixx@gmail.com>
2023-03-23 18:44:57 +0200

    checkBox, labeI i agree and regButton component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+/*
+create checkbox, text "i agree" and registration button
+if checkbox is not clicked must be disabled and it can not be clicked
+*/
+
+export default function App() {
+  const [isAgreed, setIsAgreed] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input 
+          type="checkbox"
+          onClick={(e)=>{
+            setIsAgreed(e.target.checked);
+          }}
+        
+        />
+        <label>I agree!</label>
+      </div>
+      <div>
+          <button disabled={isAgreed ? '' : 'disabled'}>
+            Registration
+          </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+
