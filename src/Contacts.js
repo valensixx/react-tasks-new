554f3bbb6422d82cf6672f51e2dbb65b35d1f644
valensixx <valensixx@gmail.com>
2023-04-02 15:08:10 +0300

    contact.js

---
+++ src/Contacts.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import './Contacts.css';
+
+ /*
+    Create Contact component like contact form in Facebook.
+    The Contact component must take image, online/offline status and name.
+    The component takes the lenght of the parent component(width: 100% of the parent).
+    Use styles in React as objects
+    All avetars are from freepic.com  
+*/
+
+export default function Contacts() {
+    return (
+        <div>
+
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import UserDetails from './UserDetails';
+import Contacts from './Contacts';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <App />
     <UserDetails />
+    <Contacts />
   </React.StrictMode>
 );
 
