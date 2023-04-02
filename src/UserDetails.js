25e08b517b812df848e4c46b21dd4552bc655499
valensixx <valensixx@gmail.com>
2023-04-02 13:33:58 +0300

    UserDetails.js

---
+++ src/UserDetails.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import React from "react";
+
+/*
+ Create component UserDetails with one user.
+ The user has name, age, and list with interests(arr['string'])
+ In tag h2 is shown name and age. In ul is shown list with interests
+*/
+
+export default function UserDetails(){
+    return(
+        <div>
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import UserDetails from './UserDetails';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <UserDetails />
   </React.StrictMode>
 );
 
