ec220503e32d4ff0e9dc6466e2562e6fd2464e28
valensixx <valensixx@gmail.com>
2023-04-18 16:14:33 +0300

    task need fixing...

--- src/InputLabel.js
+++ src/InputLabel.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import InputLabelProps from "./InputLabelProps";
 
 /*
 Create reusable label input. Input field must take text for the label above, 
@@ -7,16 +7,28 @@ text for a placeholder and function onInput
 */
 
 export default function InputLabel(props) {
-
+    const styles={
+        title:{
+            fontWeight: 700,
+            textTransform: 'uppercase',
+        },
+        input: {
+            display: 'block',
+            width: '100%',
+            padding: 10,
+            fontSize: 20,
+        }
+    }
     return (
         <div>
-            <label>
+            <label style={styles.title}>
                 {props.title}
             </label>
             <input 
                 type="text"
                 placeholder={props.placeholder}
                 onInput={props.onInput}
+                style={styles.input}
             />
         </div>
     );
