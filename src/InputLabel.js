947bb322672ab021874784e8af4848f19ec79b92
valensixx <valensixx@gmail.com>
2023-04-18 15:43:01 +0300

    InputLabel

---
+++ src/InputLabel.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+/*
+Create reusable label input. Input field must take text for the label above, 
+text for a placeholder and function onInput
+*/
+
+export default function InputLabel() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
