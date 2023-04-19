d341956118d39389402c9be59c3c34b42a3b0c51
valensixx <valensixx@gmail.com>
2023-04-19 16:50:55 +0300

    task done

--- src/MenuProps.js
+++ src/MenuProps.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function MenuProps(props) {
+    const [isShown, setIsShown] = useState(false);
     return (
         <nav className="menu">
-            <ul>
+            <button className="d-hide" onClick={() => {
+                setIsShown(!isShown);
+            }}>
+                <i className="fa fa-bars"></i>
+            </button>
+            <ul className={!isShown && "m-hide"}>
                 {props.items.map(item =>
                     <li>
                         <a href={item.link}>{item.label}</a>
