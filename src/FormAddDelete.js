5bed86ea44c5f13ed625ca2e585d9184dcf2a0c7
valensixx <valensixx@gmail.com>
2023-04-19 14:46:43 +0300

    AddDeleteForm

---
+++ src/FormAddDelete.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+/*
+Create form with select. Data must be added from array. 
+Every array contains label and value. Also add and delete button.
+Pressing the button, in ul, will add element with delete option.
+*/
+
+export default function Form(){
+    return(
+        <div>
+
+        </div>
+    );
+}
